579df81cdbd32e1ee9cf9bdaede62ed4756344a2
Abhay <abhayparasharr@gmail.com>
2023-09-18 08:53:57 +0530

    Add custom user model, and modify docker-compose to wait_for_db and migrate when running the app service. Also add wait_for_db in the workflow run process to ensure database availability

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,48 @@
-from django.db import models  # noqa
+"""
+Models definition for APIs.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for the user model."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates users in the system."""
+        if not email:
+            raise ValueError('Email is required for user creation')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_super_user(self, **user_data):
+        """Creates super user in the system. """
+        user = self.create_user(**user_data)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Model for users."""
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    bio = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=10, blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,70 @@
+"""
+Tests for checking models.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Tests for models"""
+
+    def test_create_user_with_valid_data_pass(self):
+        """Test creating a user is successful with valid data."""
+        user_data = {
+            'email': 'test@example.com',
+            'name': 'Test Example',
+            'bio': 'Test Bio',
+            'phone': '1234567890',
+        }
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            **user_data, password=password
+        )
+
+        for attr, value in user_data.items():
+            self.assertEqual(getattr(user, attr), value)
+
+        self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalized(self):
+        """Test that user's email is normalized."""
+        sample_emails = [
+            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['test2@ExaMple.com', 'test2@example.com'],
+            ['Test3@Example.com', 'Test3@example.com'],
+            ['TEST4@example.COM', 'TEST4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email, password='test@123'
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_create_user_without_email_raises_error(self):
+        """Test creating a user without email raises a value error."""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                '',
+                password='test1234'
+            )
+
+    def test_default_create_user_is_not_super_user(self):
+        """Test that users created using create_user
+        are not super users by default."""
+        user = get_user_model().objects.create_user(
+            email='test1@example.com', password='test123'
+        )
+
+        self.assertFalse(user.is_superuser)
+
+    def test_create_super_user(self):
+        """Test creating super users is successful."""
+        user = get_user_model().objects.create_super_user(
+            email='test1@example.com', password='test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
